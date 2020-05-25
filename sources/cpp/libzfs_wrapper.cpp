870df2c96924be650df62b918b6fc618f529c5e7
Ivan Smirnov <ivsmir@gmail.com>
2020-05-25 08:56:50 +0300

    ZFS init and fini added

--- sources/cpp/libzfs_wrapper.cpp
+++ sources/cpp/libzfs_wrapper.cpp
@@ -2,11 +2,32 @@
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(zfswrapper, m) {
-    m.doc() = "ZFS binding module";
-    m.def("test", &test, "Test function");
+PYBIND11_MODULE(zfswrapper, lib) {
+    lib.doc() = "ZFS binding module";
+
+    lib.def("zfs_init", &zfs_init, "Library initialization")
+    lib.def("zfs_fini", &zfs_init, "Deallocate resources")
 };
 
-int test(char *message) {
-    return 42;
+void* zfs_init(void) {
+    return libzfs_init();
 }
+
+void zfs_fini(void* ptr) {
+    libzfs_fini(ptr);
+}
+
+
+/*
+
+typedef struct zfs_handle zfs_handle_t;
+typedef struct zpool_handle zpool_handle_t;
+typedef struct libzfs_handle libzfs_handle_t;
+
+extern libzfs_handle_t *libzfs_init(void);
+extern void libzfs_fini(libzfs_handle_t *);
+
+extern libzfs_handle_t *zpool_get_handle(zpool_handle_t *);
+extern libzfs_handle_t *zfs_get_handle(zfs_handle_t *);
+
+*/
