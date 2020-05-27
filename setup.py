652f83d30b2cf952a98fb8eda0acfd1efddd1fb5
Ivan Smirnov <ivsmir@gmail.com>
2020-05-27 23:31:55 +0300

    GPLv3 license notification added

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+#   Copyright (C) 2020  Ivan Smirnov <ivsmir@gmail.com>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not see see <http://www.gnu.org/licenses/>
+
 """
 .. moduleauthor:: Ivan Smirnov <ivsmir@gmail.com>
 """
 
 from distutils.core import setup, Extension
-from doc.source.conf import project, release
 import pybind11
+from doc.source.conf import project, release
 
 modules = [
     Extension(
@@ -25,7 +40,7 @@ modules = [
             '/usr/include/libspl',
             '/usr/include/lib',
         ],
-        library_dirs = [
+        library_dirs=[
             '/usr/lib',
         ],
         libraries=['zfs', 'zpool', 'nvpair'],
@@ -33,8 +48,8 @@ modules = [
 ]
 
 setup(
-    name = project,
-    version = release,
-    ext_modules = modules,
-    package_data = {'': ['lib/*.so']},
+    name=project,
+    version=release,
+    ext_modules=modules,
+    package_data={'': ['lib/*.so']},
 )
